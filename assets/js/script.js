df797315c0b6f7a655acc2eba2593f3dad692b9b
Yazminserranos <yazmin.serrano.s@gmail.com>
2022-10-26 19:22:30 -0700

    added code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,45 @@
 // TODO: Declare any global variables we need
+let flipresult;
+let numFlips;
+let percHeads;
+let numbHeads;
+let numTails;
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
+//Dom Elements
+let pennyImg="";
+let tailsImg ="";
+let statusMsgDiv= document.createElement('div');
+let percHeadsBox = document.querySelector("#heads-percent");
+let percTailsBox = document.querySelector("#tails-percent");
+let numHeadsBox = document.querySelector("#heads");
+let numTailsBox= document.querySelector("#Tails");
+let flipBtn = document.createElement("#flip");
+let clearBtn = document.createElement("#clear");
+
+flipBtn.addEventListener('DOMContentLoaded', () => {
+document.getElementById('flip').addEventListener('click', () => {
+    let flipresult = Math.random() < 0.5
+
+
+if (flip<0.5) {
+    Image.src = tailsImg;
+    statusMsgDiv.textContent= "you flipped heads";
+} else if (flip >= 0.5) {
+    Image.src = headsImg;
+    statusMsgDiv.textContent = "you Flipped Heads";
+    numFlips += 1;
+    numHeads += 1;
+
+    percHeadsBox.textContent= "";
+    percTailsBox;
+    numHeadsBox;
+    numTailsBox;
+}
+}
+
+
+}
 
     // TODO: Add event listener and handler for flip and clear buttons
 
